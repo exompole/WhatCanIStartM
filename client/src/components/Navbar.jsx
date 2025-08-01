e3765f3ca8cda56113a4e789cada63c1788382f0
nupur <nupurshankhapal@gmail.com>
2025-08-01 13:59:17 +0530

    Fix rollup optional deps issue

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import { Link, useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
 import styles from "./Navbar.module.css";
-import logo from "../images/logo.png";
+import logo from "../images/Logo.png";
 
 const Navbar = () => {
   const navigate = useNavigate();
